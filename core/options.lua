87337833e7479f9bb4255ef43fc5d53e83b7f1c4
Catok <anloren@gmail.com>
2012-12-28 11:56:18 +0100

    Forgot to stage.

--- core/options.lua
+++ core/options.lua
@@ -2,154 +2,34 @@ local E, L, V, P, G,_ = unpack(ElvUI)
 local AS = E:GetModule('AddOnSkins')
 
 local Skins = {
-	["ACPSkin"] = {
-		["buttonText"] = "Addon Control Panel",
-		["addon"] = "ACP",
-	},
-	["AdiBagsSkin"] = {
-		["buttonText"] = "AdiBags",
-		["addon"] = "AdiBags",
-	},
 	["AltoholicSkin"] = {
 		["addon"] = "Altoholic",
-	},
-	["AraBrokerGuildFriendsSkin"] = {
-		["buttonText"] = "Ara Broker Guild Friends",
-		["addon"] = "Ara_Broker_Guild_Friends"
-	},
-	["ArchySkin"] = {
-		["addon"] = "Archy"
-	},
-	['ArhSkin'] = {
-		["buttonText"] = "ArchaeologyHelper",
-		["addon"] = "Arh"
-	},
-	["ArkInventorySkin"] = {
-		["addon"] = "ArkInventory"
 	},
 	["AtlasLootSkin"] = {
 		["buttonText"] = "AtlasLoot",
 		["addon"] = "AtlasLoot_Loader"
 	},
-	["ATSWSkin"] = {
-		["addon"] = "AdvancedTradeSkillWindow"
-	},
 	["AuctionatorSkin"] = {
 		["addon"] = "Auctionator"
-	},
-	["AuctioneerSkin"] = {
-		["buttonText"] = "Auctioneer",
-		["addon"] = "Auc-Advanced"
 	},
 	["BPTSkin"] = {
 		["buttonText"] = "Balance Power Tracker",
 		["addon"] = "BalancePowerTracker"
 	},
-	["BGDefenderSkin"] = {
-		["addon"] = "BGDefender"
-	},
 	["BigWigsSkin"] = {
 		["addon"] = "BigWigs"
-	},
-	["BuyEmAllSkin"] = {
-		["addon"] = "BuyEmAll"
-	},
-	["CLCInfoSkin"] = {
-		["addon"] = "CLCInfo",
-		["ui"] = "ElvUI"
-	},
-	["CLCProtSkin"] = {
-		["addon"] = "CLCProt",
-		["ui"] = "ElvUI"
-	},
-	["CLCRetSkin"] = {
-		["addon"] = "CLCRet",
-		["ui"] = "ElvUI"
 	},
 	["CliqueSkin"] = {
 		["addon"] = "Clique"
 	},
-	["DBMSkin"] = {
-		["buttonText"] = "DBM",
-		["addon"] = "DBM-Core"
-	},
-	["EasyMailSkin"] = {
-		["addon"] = "EasyMail"
-	},
-	["EnergyWatchSkin"] = {
-		["addon"] = "EnergyWatch"
-	},
-	["ExtVendorSkin"] = {
-		["buttonText"] = "Extended Vendor",
-		["addon"] = "ExtVendor"
-	},
 	["FactionizerSkin"] = {
 		["addon"] = "Factionizer"
-	},
-	["FlightMapSkin"] = {
-		["addon"] = "FlightMap"
-	},
-	["IncomingSkin"] = {
-		["addon"] = "Incoming"
-	},
-	["KarniCrapSkin"] = {
-		["buttonText"] = "Karni's Crap Filter",
-		["addon"] = "KarniCrap"
-	},
-	["LightheadedSkin"] = {
-		["addon"] = "Lightheaded"
-	},
-	["LootCouncilLiteSkin"] = {
-		["buttonText"] = "LootCouncilLite",
-		["addon"] = "LootCouncil_Lite"
-	},
-	["MageNuggetsSkin"] = {
-		["addon"] = "MageNuggets"
-	},
-	["MasterLootManagerRemixSkin"] = {
-		["addon"] = "MasterLootManagerRemix"
-	},
-	["MinimalArchaeologySkin"] = {
-		["addon"] = "MinimalArchaeology"
-	},
-	["MoveAnythingSkin"] = {
-		["addon"] = "MoveAnything"
-	},
-	["MRTSkin"] = {
-		["buttonText"] = "Mizus Raid Tracker",
-		["addon"] = "MizusRaidTracker"
-	},
-	["MyRolePlaySkin"] = {
-		["addon"] = "MyRolePlay"
-	},
-	["OdysseySkin"] = {
-		["addon"] = "Odyssey"
-	},
-	["OgriLazySkin"] = {
-		["addon"] = "Ogri'Lazy"
 	},
 	["OmenSkin"] = {
 		["addon"] = "Omen"
 	},
-	["OutfitterSkin"] = {
-		["addon"] = "Outfitter"
-	},
-	["PoMTrackerSkin"] = {
-		["addon"] = "PoMTracker"
-	},
-	["PostalSkin"] = {
-		["addon"] = "Postal"
-	},
 	["QuartzSkin"] = {
 		["addon"] = "Quartz"
-	},
-	["RaidInviteOrganizerSkin"] = {
-		["buttonText"] = "Raid Invite Organizer",
-		["addon"] = "RaidInviteOrganizer"
-	},
-	["RaidBuffStatusSkin"] = {
-		["buttonText"] = "Raid Buff Status",
-		["addon"] = "RaidBuffStatus"
 	},
 	["RecountSkin"] = {
 		["addon"] = "Recount"
@@ -157,26 +37,8 @@ local Skins = {
 	["ReforgenatorSkin"] = {
 		["addon"] = "Reforgenator"
 	},
-	["SearingPlasmaTrackerSkin"] = {
-		["buttonText"] = "Searing Plasma Tracker",
-		["addon"] = "SearingPlasmaTracker"
-	},
-	["SexyCooldownSkin"] = {
-		["buttonText"] = "SexyCooldown",
-		["addon"] = "SexyCooldown2"
-	},
 	["SkadaSkin"] = {
 		["addon"] = "Skada"
-	},
-	["SkilletSkin"] = {
-		["addon"] = "Skillet"
-	},
-	["SpineCounterSkin"] = {
-		["buttonText"] = "Spine Blood Counter",
-		["addon"] = "SpineCounter"
-	},
-	["SpySkin"] = {
-		["addon"] = "Spy"
 	},
 	["SwatterSkin"] = {
 		["buttonText"] = "Swatter",
@@ -184,12 +46,6 @@ local Skins = {
 	},
 	["TellMeWhenSkin"] = {
 		["addon"] = "TellMeWhen"
-	},
-	["TinyDPSSkin"] = {
-		["addon"] = "TinyDPS"
-	},
-	["TradeSkillDWSkin"] = {
-		["addon"] = "TradeSkillDW"
 	},
 	["WeakAurasSkin"] = {
 		["addon"] = "WeakAuras",
@@ -209,18 +65,11 @@ local Skins = {
 		["buttonText"] = "PetBattleTeams",
 		["addon"] = "PetBattleTeams"
 	},
-	["BagnonSkin"] = {
-		["addon"] = "Bagnon",
-	},
 	["VengeanceStatusSkin"] = {
 		["addon"] = "VengeanceStatus",
 	},
 	["TomTomSkin"] = {
 		["addon"] = "TomTom",
-	},
-	["ShieldMonitorSkin"] = {
-		["buttonText"] = "Shield Monitor",
-		["addon"] = "shieldmonitor",
 	},
 }
 
